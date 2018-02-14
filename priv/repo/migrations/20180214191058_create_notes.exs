5d32a913e4c0efe501f634b5bfac695b74b4f156
shouston3 <shouston3@me.com>
2018-02-14 19:35:06 +0000

    mix phx.gen.schema Note notes cid:integer nid:integer cmod:integer nmod:integer mid:integer tags:array:string flds:string sfld:string did:integer ord:integer type:integer queue:integer due:integer reps:integer lapses:integer

---
+++ priv/repo/migrations/20180214191058_create_notes.exs
@@ -0,0 +1,26 @@
+defmodule AnkiViewer.Repo.Migrations.CreateNotes do
+  use Ecto.Migration
+
+  def change do
+    create table(:notes) do
+      add :cid, :integer
+      add :nid, :integer
+      add :cmod, :integer
+      add :nmod, :integer
+      add :mid, :integer
+      add :tags, {:array, :string}
+      add :flds, :string
+      add :sfld, :string
+      add :did, :integer
+      add :ord, :integer
+      add :type, :integer
+      add :queue, :integer
+      add :due, :integer
+      add :reps, :integer
+      add :lapses, :integer
+
+      timestamps()
+    end
+
+  end
+end
