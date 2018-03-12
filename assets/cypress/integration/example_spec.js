fa71c234b4e9fc2da90e851e77173567fc21e3a9
shouston3 <shouston3@me.com>
2018-03-12 23:14:01 +0000

    configure travis to start test server and for cypress to test it

--- assets/cypress/integration/example_spec.js
+++ assets/cypress/integration/example_spec.js
@@ -1,6 +1,6 @@
 describe('Kitchen Sink', function () {
   it('.should() - assert that <title> is correct', function () {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:4001')
     cy.title().should('include', 'Kitchen Sink')
   })
 })
