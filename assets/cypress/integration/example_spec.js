c72f3cd9ffb3b3deb002ee1223392c82f1ba70c0
shouston3 <shouston3@me.com>
2018-03-13 20:31:56 +0000

    fix cypess test

--- assets/cypress/integration/example_spec.js
+++ assets/cypress/integration/example_spec.js
@@ -1,6 +1,6 @@
 describe('Kitchen Sink', function () {
   it('.should() - assert that <title> is correct', function () {
     cy.visit('http://localhost:4001')
-    cy.title().should('include', 'Kitchen Sink')
+    cy.title().should('include', 'Hello AnkiViewer!')
   })
 })
