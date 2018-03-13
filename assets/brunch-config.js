f5e4f25d4d4acb710f11d746e029213e820ff3e0
shouston3 <shouston3@me.com>
2018-03-13 20:59:14 +0000

    brunch elm setup as well as elm travis set up

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -37,7 +37,7 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
+    watched: ["static", "css", "js", "elm", "vendor"],
     // Where to compile files to
     public: "../priv/static"
   },
@@ -46,7 +46,14 @@ exports.config = {
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/vendor/]
+      ignore: [/vendor/, /main\.js/]
+    },
+    elmBrunch: {
+      mainModules: ["elm/Main.elm"],
+      elmFolder: ".",
+      executablePath: "node_modules/elm/binwrappers",
+      outputFolder: "js",
+      outputFile: "main.js"
     }
   },
 
