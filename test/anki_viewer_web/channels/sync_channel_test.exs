0731124d7c9f2a828a52ec145b5cda90b8e3a723
shouston3 <shouston3@me.com>
2018-03-17 00:24:47 +0000

    set up sqlite before tests and add basic channel tests

--- test/anki_viewer_web/channels/sync_channel_test.exs
+++ test/anki_viewer_web/channels/sync_channel_test.exs
@@ -3,30 +3,17 @@ defmodule AnkiViewerWeb.SyncChannelTest do
 
   alias AnkiViewerWeb.SyncChannel
 
-  @tmp System.get_env("TMP_DIR") || "/tmp"
-
   describe "sync:database" do
     setup do
-      System.cmd("sqlite3", ["#{@tmp}/anki_test.db", "'.read #{:code.priv_dir(:anki_viewer)}/anki_test_dump.sql'"])
       {:ok, _, socket} = subscribe_and_join(socket(), SyncChannel, "sync:database")
 
       {:ok, socket: socket}
     end
 
-    test "ping replies with status ok", %{socket: _socket} do
-      assert true
-    end
-  end
-
-  describe "sync:rule" do
-    setup do
-      {:ok, _, socket} = subscribe_and_join(socket(), SyncChannel, "sync:rule", %{id: 1})
-
-      {:ok, socket: socket}
-    end
-
-    test "hello:world" do
-      assert true
+    test "socket pushes 3 messages", %{socket: _socket} do
+      assert_push("one", %{})
+      assert_push("two", %{})
+      assert_push("three", %{})
     end
   end
 end
