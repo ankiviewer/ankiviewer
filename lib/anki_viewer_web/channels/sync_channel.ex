39fa1e57405bc397bc3713fe3f1e5d828b2fbf90
shouston3 <shouston3@me.com>
2018-03-17 11:11:23 +0000

    allow for increasing the 5 second timeout in tests

--- lib/anki_viewer_web/channels/sync_channel.ex
+++ lib/anki_viewer_web/channels/sync_channel.ex
@@ -2,16 +2,16 @@ defmodule AnkiViewerWeb.SyncChannel do
   use AnkiViewerWeb, :channel
 
   def join("sync:database", _payload, socket) do
-    send(self(), {:sync, :database})
+    Process.send_after(self(), {:sync, :database}, 100)
 
     {:ok, socket}
   end
 
   def handle_info({:sync, :database}, socket) do
     push(socket, "one", %{})
-    Process.sleep(500)
+    Process.sleep(3000)
     push(socket, "two", %{})
-    Process.sleep(500)
+    Process.sleep(3000)
     push(socket, "three", %{})
 
     {:noreply, socket}
