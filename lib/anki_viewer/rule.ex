80b1e58a4ba0d26d8e6957ce6eceb312f03fc66f
shouston3 <shouston3@me.com>
2018-03-20 22:57:08 +0000

    add rule with rid primary key

--- lib/anki_viewer/rule.ex
+++ lib/anki_viewer/rule.ex
@@ -1,9 +1,9 @@
 defmodule AnkiViewer.Rule do
   use Ecto.Schema
   import Ecto.Changeset
-  alias AnkiViewer.Rule
+  alias AnkiViewer.{Rule, Repo}
 
-  @primary_key {:rid, :id, autoincrement: true}
+  @primary_key {:rid, :id, autogenerate: true}
   schema "rules" do
     field(:code, :string)
     field(:tests, :string)
@@ -18,4 +18,17 @@ defmodule AnkiViewer.Rule do
     |> cast(attrs, @attrs)
     |> validate_required(@attrs)
   end
+
+  def insert!(attrs) when is_map(attrs) do
+    attrs
+    |> Map.has_key?(:__struct__)
+    |> case do
+      true -> attrs
+      false -> Map.merge(%Rule{}, attrs)
+    end
+    |> changeset
+    |> Repo.insert!()
+  end
+
+  def insert!(list) when is_list(list), do: list |> Enum.each(&insert!/1)
 end
