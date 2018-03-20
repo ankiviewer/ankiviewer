d7dc632e4e65de2c8fad493832422983c0540063
shouston3 <shouston3@me.com>
2018-03-20 21:33:12 +0000

    initial rule test

---
+++ test/anki_viewer/rule_test.exs
@@ -0,0 +1,37 @@
+defmodule AnkiViewer.RuleTest do
+  use AnkiViewer.DataCase, async: false
+
+  test "insert" do
+    attrs = %{
+      name: "duplicate front",
+      code: "(deck, note) => {
+        var fine = true;
+        for (var i=0;i<=deck.length;i++) {
+          if (deck[i].sfld == note.sfld && deck[i].nid != note.nid) {
+            fine = false;
+            break;
+          }
+        }
+        return fine;
+      }",
+      tests: "[
+        {
+          deck: [
+            {nid: 0, sfld: 'h'},
+            {nid: 1, sfld: 'hi'}
+          ],
+          note: {nid: 0, sfld: 'h'},
+          fine: true
+        },
+        {
+          deck: [
+            {nid: 0, sfld: 'h'},
+            {nid: 1, sfld: 'h'},
+          ]
+        }
+      ]"
+    }
+
+    IO.inspect(attrs)
+  end
+end
