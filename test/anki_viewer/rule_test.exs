87f9f429934ca459f8edd235bdd3b6d28f607a49
shouston3 <shouston3@me.com>
2018-03-20 23:22:28 +0000

    run tests rule function

--- test/anki_viewer/rule_test.exs
+++ test/anki_viewer/rule_test.exs
@@ -1,40 +1,92 @@
 defmodule AnkiViewer.RuleTest do
   use AnkiViewer.DataCase, async: false
 
-  test "insert" do
-    attrs = %{
-      name: "duplicate front",
-      code: "(deck, note) => {
-        var fine = true;
-        for (var i=0;i<=deck.length;i++) {
-          if (deck[i].sfld == note.sfld && deck[i].nid != note.nid) {
-            fine = false;
-            break;
+  describe "rule" do
+    setup do
+      attrs = [
+        %{
+          name: "no duplicate front",
+          code: """
+          (deck, note) => {
+            var fine = true;
+            for (var i=0;i<=deck.length;i++) {
+              if (deck[i].sfld == note.sfld && deck[i].nid != note.nid) {
+                fine = false;
+                break;
+              }
+            }
+            return fine;
           }
-        }
-        return fine;
-      }",
-      tests: "[
-        {
-          deck: [
-            {nid: 0, sfld: 'h'},
-            {nid: 1, sfld: 'hi'}
-          ],
-          note: {nid: 0, sfld: 'h'},
-          fine: true
-        },
-        {
-          deck: [
-            {nid: 0, sfld: 'h'},
-            {nid: 1, sfld: 'h'},
+          """,
+          tests: """
+          [
+            {
+              "deck": [
+                {"nid": 0, "sfld": "h"},
+                {"nid": 1, "sfld": "hi"}
+              ],
+              "note": {"nid": 0, "sfld": "h"},
+              "fine": true
+            },
+            {
+              "deck": [
+                {"nid": 0, "sfld": "h"},
+                {"nid": 1, "sfld": "h"},
+              ],
+              "note": {"nid": 0, "sfld": "h"},
+              "fine": true
+            }
           ]
+          """
+        },
+        %{
+          name: "nouns start with 'the'",
+          code: """
+          (_deck, note) => {
+            if (note.tags.includes('noun')) {
+              return note.sfld.startsWith('the ')
+            } else {
+              return true;
+            }
+          }
+          """,
+          tests: """
+          [
+            {
+              "note": {"tags": ["noun"], "sfld": "h"},
+              "fine": false
+            },
+            {
+            "note": {"tags": [], "sfld": "h"},
+            "fine": true
+            },
+            {
+            "note": {"tags": ["noun"], "sfld": "the h"},
+            "fine": true
+            }
+          ]
+          """
         }
-      ]"
-    }
+      ]
 
-    Rule.insert!(attrs)
+      Rule.insert!(attrs)
+    end
 
-    [%Rule{name: name}] = Repo.all(Rule)
-    assert name == "duplicate front"
+    test "insert" do
+      [%Rule{name: name} | _] = Repo.all(Rule)
+      assert name == "no duplicate front"
+    end
+
+    test "run tests through code" do
+      ok_rule = Repo.get_by(Rule, name: "nouns start with 'the'")
+
+      {:ok, ""} = Rule.run_tests(ok_rule)
+
+      error_rule = Repo.get_by(Rule, name: "no duplicate front")
+
+      {:error, error} = Rule.run_tests(error_rule)
+
+      assert error == ""
+    end
   end
 end
