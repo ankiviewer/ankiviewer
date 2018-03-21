14892d729ad13073d416c75e07fd9135ddc567b4
shouston3 <shouston3@me.com>
2018-03-21 22:47:46 +0000

    set up of note rule

--- lib/anki_viewer/note_rule.ex
+++ lib/anki_viewer/note_rule.ex
@@ -1,7 +1,7 @@
 defmodule AnkiViewer.NoteRule do
   use Ecto.Schema
   import Ecto.Changeset
-  alias AnkiViewer.NoteRule
+  alias AnkiViewer.{Note, Rule, NoteRule, Repo}
 
   schema "note_rules" do
     field(:comment, :string)
@@ -17,9 +17,43 @@ defmodule AnkiViewer.NoteRule do
 
   @required_params ~w(fails nid rid)a
   @optional_params ~w(comment solution url ignore)a
-  def changeset(%NoteRule{} = note_rule, attrs) do
+  def changeset(%NoteRule{} = note_rule, attrs \\ %{}) do
     note_rule
     |> cast(attrs, @required_params ++ @optional_params)
     |> validate_required(@required_params)
   end
+
+  def run(notes, %Note{} = note, %Rule{code: code}) do
+    rule_func = """
+    const ruleFunc = #{code};
+    const notes = process.argv[1];
+    const note = process.argv[2];
+    JSON.parse(notes).forEach((note) => {
+      if (!ruleFunc(notes, note)) {
+        process.exit(1);
+      }
+    });
+    """
+
+    notes = notes |> Utils.parseable_fields() |> Jason.encode!()
+    note = note |> Utils.parseable_fields() |> Jason.encode!()
+
+    case System.cmd("node", ["-e", rule_func, notes, note]) do
+      {"", 0} -> :ok
+      {error, 1} -> {:error, error}
+    end
+  end
+
+  def insert!(attrs) when is_map(attrs) do
+    attrs
+    |> Map.has_key?(:__struct__)
+    |> case do
+      true -> attrs
+      false -> Map.merge(%NoteRule{}, attrs)
+    end
+    |> changeset
+    |> Repo.insert!()
+  end
+
+  def insert!(list) when is_list(list), do: list |> Enum.each(&insert!/1)
 end
