02e4e3713554bd0fd67d5bc1c317ef14445c73e3
shouston3 <shouston3@me.com>
2018-03-22 22:09:47 +0000

    add elm formatting

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -1,6 +1,45 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, program, text, div, button)
+import Html.Events exposing (onClick)
 
 
-main = text "Hello world"
+main =
+    program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+type Msg
+    = NoOp
+
+
+type alias Model =
+    { txt : String }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
+initialModel : Model
+initialModel =
+    { txt = "hello world" }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            model ! []
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick NoOp ] [ text "Load Database" ]
+        ]
