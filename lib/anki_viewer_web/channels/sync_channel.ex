76d92f57f73e7b59954357b2258055f83b881351
shouston3 <shouston3@me.com>
2018-03-23 21:05:03 +0000

    sync notes

--- lib/anki_viewer_web/channels/sync_channel.ex
+++ lib/anki_viewer_web/channels/sync_channel.ex
@@ -13,20 +13,21 @@ defmodule AnkiViewerWeb.SyncChannel do
   def handle_info({:sync, :database}, socket) do
     push(socket, "sync:msg", %{msg: "starting sync"})
 
-    {x, 0} =
+    {collection_data, 0} =
       System.cmd("sqlite3", [@anki_db_path, "select crt, mod, models, decks, tags from col"])
 
-    {_y, 0} =
+    {notes_data, 0} =
       System.cmd("sqlite3", [
+        "-header",
         @anki_db_path,
         "select c.id as cid, c.mod as cmod, c.did as did, c.due as due, n.flds as flds, c.lapses as lapses, n.mid as mid, n.id as nid, n.mod as nmod, c.ord as ord, c.queue as queue, c.reps as reps, n.sfld as sfld, n.tags as tags, c.type as type from notes as n inner join cards as c on n.id = c.nid"
       ])
 
-    [crt, mod, models, decks, tags] = String.split(x, "|")
+    [crt, mod, models, decks, tags] = String.split(collection_data, "|")
 
     %Collection{
-      crt: String.to_integer(crt),
-      mod: mod |> String.to_integer() |> sanitize_integer(),
+      crt: sanitize_integer(crt),
+      mod: sanitize_integer(mod),
       tags: tags |> Jason.decode!() |> Map.keys()
     }
     |> Collection.insert_or_update!()
@@ -67,6 +68,36 @@ defmodule AnkiViewerWeb.SyncChannel do
     Deck.insert_or_update!(deck_attrs)
 
     push(socket, "sync:msg", %{msg: "updated collection"})
+
+    [keys | notes_data] =
+      notes_data
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.split(&1, "|"))
+
+    Repo.delete_all(Note)
+
+    notes =
+      notes_data
+      |> Enum.map(fn nd ->
+        nd
+        |> Enum.with_index()
+        |> Map.new(fn {n, i} ->
+          case keys |> Enum.at(i) |> String.to_atom() do
+            :sfld -> {:sfld, n}
+            :flds -> {:flds, n}
+            k -> {k, sanitize_integer(n)}
+          end
+        end)
+        |> Map.update!(:tags, &String.split(&1, " ", trim: true))
+      end)
+
+    for {n, i} <- Enum.with_index(notes) do
+      push(socket, "sync:msg", %{msg: "updating note #{i}/#{length(notes)}"})
+      Note.insert!(n)
+    end
+
+    push(socket, "sync:msg", %{msg: "updated notes"})
+
     push(socket, "done", %{})
 
     {:noreply, socket}
