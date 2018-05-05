57c90c2f8345d9eeb7fd787c55d08fa0c360aaab
shouston3 <shouston3@me.com>
2018-05-05 23:22:43 +0100

    create search page

--- test/anki_viewer_web/controllers/page_controller_test.exs
+++ test/anki_viewer_web/controllers/page_controller_test.exs
@@ -7,7 +7,7 @@ defmodule AnkiViewerWeb.PageControllerTest do
     assert html_response(conn, 200)
   end
 
-  describe "GET /collection" do
+  describe "GET /api/collection" do
     test "with no collection data", %{conn: conn} do
       conn = get(conn, "/api/collection")
 
@@ -19,6 +19,81 @@ defmodule AnkiViewerWeb.PageControllerTest do
       conn = get(conn, "/api/collection")
 
       assert json_response(conn, 200) == %{"mod" => 1_514_655_628, "notes" => 10}
+    end
+  end
+
+  describe "GET /api/notes" do
+    test "with bad params", %{conn: conn} do
+      conn = get(conn, "/api/notes")
+
+      assert json_response(conn, 200) == %{"error" => "BAD PARAMS"}
+    end
+
+    test "without notes data", %{conn: conn} do
+      params =  ~s(search=&model=&deck=&tags=&modelorder=&rule=)
+
+      conn = get(conn, "/api/notes?" <> params)
+
+      assert json_response(conn, 200) == []
+    end
+
+    test "with notes data", %{conn: conn} do
+      load_collection!()
+
+      params = ~s(search=&model=&deck=&tags=&modelorder=&rule=)
+
+      conn = get(conn, "/api/notes?" <> params)
+
+      assert json_response(conn, 200) |> length == 10
+    end
+
+    test "with deck name", %{conn: conn} do
+      load_collection!()
+
+      params = ~s(search=&model=&deck=DE&tags=&modelorder=&rule=)
+
+      conn = get(conn, "/api/notes?" <> params)
+
+      assert json_response(conn, 200) |> length == 10
+    end
+
+    test "with search", %{conn: conn} do
+      load_collection!()
+
+      notes = [
+        %{
+          "deck" => "DE",
+          "due" => 415,
+          "flds" => "Die Einigungthe agreement",
+          "lapses" => 0,
+          "model" => "de_reverse",
+          "ord" => 0,
+          "queue" => 2,
+          "reps" => 3,
+          "sfld" => "the agreement",
+          "tags" => [],
+          "type" => 2
+        },
+        %{
+          "deck" => "DE",
+          "due" => 395,
+          "flds" => "Die Einigungthe agreement",
+          "lapses" => 2,
+          "model" => "de_reverse",
+          "ord" => 1,
+          "queue" => 2,
+          "reps" => 10,
+          "sfld" => "the agreement",
+          "tags" => [],
+          "type" => 2
+        }
+      ]
+
+      params = ~s(search=agreement&model=&deck=&tags=&modelorder=&rule=)
+
+      conn = get(conn, "/api/notes?" <> params)
+
+      assert json_response(conn, 200) == notes
     end
   end
 end
