1f6efe92aabdf356e88f57903d08033d9628c4e6
shouston3 <shouston3@me.com>
2018-06-23 23:47:00 +0100

    working routing for app

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,9 @@ import Elm from './elm.js';
 
 var app_node = document.querySelector('#app');
 var app = Elm.Main.embed(app_node);
+
+app.ports.url.subscribe(function (url) {
+  console.log(url);
+});
+
+app.ports.url.send({url: '/'});
