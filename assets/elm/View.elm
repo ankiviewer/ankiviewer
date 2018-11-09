4c3d155935a0b7e27ff3c1875e430ceb9fca49af
samhstn <sam.houston@elixirrcreative.com>
2018-11-09 13:20:55 +0000

    using samhstn/time-format module for time parsing

--- assets/elm/View.elm
+++ assets/elm/View.elm
@@ -2,23 +2,13 @@ module View exposing (view)
 
 import Html exposing (Html, text, div)
 import Types exposing (Model, Msg)
-import Time
-import DateString
-
-
-{-|Convert an int to a date string
-Examples:
-> dateString 1540216129594
-"Monday 1st November 2018"
--}
-dateString : Int -> String
-dateString =
-    DateString.format "dddd, Do MMMM YYYY" 
+import Time exposing (utc)
+import Time.Format as Time
 
 
 view : Model -> Html Msg
 view {collection} =
     div
         []
-        [ text <| dateString collection.mod
+        [ text <| Time.format utc "Weekday, ordDay Month Year at padHour:padMinute" collection.mod
         ]
