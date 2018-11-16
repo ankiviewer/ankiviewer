206d13c900cf23f26a385503be361d0e53fff71b
samhstn <shouston3@me.com>
2018-11-16 17:39:03 +0000

    configure cards syncing

--- assets/elm/View.elm
+++ assets/elm/View.elm
@@ -1,9 +1,9 @@
-module View exposing (body, errorView, homeInfo, homePage, navbar, notFoundPage, pageToString, rulesPage, searchPage, view)
+module View exposing (body, errorView, homeInfo, homePage, navbar, notFoundPage, pageToString, rulesPage, searchPage, syncing, view)
 
 import Browser
 import Browser.Navigation as Nav
 import Html exposing (Html, a, button, div, text)
-import Html.Attributes exposing (class, classList, href)
+import Html.Attributes exposing (class, classList, href, style)
 import Html.Events exposing (onClick)
 import Time
 import Time.Format as Time
@@ -79,13 +79,39 @@ homePage ({ collection } as model) =
 
         None ->
             if model.isSyncing then
-                div
-                    []
-                    [ text "isSyncing" ]
+                syncing
+                    { message = model.incomingMsg
+                    , syncPercentage = model.syncPercentage
+                    }
 
             else
                 homeInfo
-                    { mod = collection.mod, cards = collection.cards }
+                    { mod = collection.mod
+                    , cards = collection.cards
+                    }
+
+
+syncing : { message : String, syncPercentage : Int } -> Html Msg
+syncing { message, syncPercentage } =
+    div
+        []
+        [ div
+            []
+            [ text <| message ++ "..."
+            ]
+        , div
+            [ class "sync-loader" ]
+            [ div
+                [ class "sync-bar"
+                , style "width" <| String.fromInt syncPercentage ++ "%"
+                ]
+                []
+            ]
+        , div
+            []
+            [ text <| String.fromInt syncPercentage ++ "%"
+            ]
+        ]
 
 
 homeInfo : { mod : Int, cards : Int } -> Html Msg
@@ -101,7 +127,9 @@ homeInfo { mod, cards } =
             [ text <| "Number notes: " ++ String.fromInt cards
             ]
         , button
-            [ onClick StartSync ]
+            [ onClick StartSync
+            , class "button-primary"
+            ]
             [ text "Sync Database"
             ]
         ]
