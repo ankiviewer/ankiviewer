9bb473f57a1d04a67b0ceb83c47195d5825cedf2
samhstn <sam.houston@elixirrcreative.com>
2018-11-25 13:40:39 +0000

    add cypress tests and update documentation

--- assets/cypress/integration/routes_spec.js
+++ assets/cypress/integration/routes_spec.js
@@ -2,18 +2,18 @@ describe('Routes', () => {
   it('Home should have the correct <title>', () => {
     cy.visit('/');
     cy.title().should('include', 'Ankiviewer - Home');
-    cy.get('.button-primary').should('contain', 'Sync Database');
+    cy.get('.nav-item.selected').should('contain', 'Home');
   });
 
   it('Search should have the correct <title>', () => {
     cy.visit('/search');
     cy.title().should('include', 'Ankiviewer - Search');
-    cy.get('button').should('contain', 'Edit columns');
+    cy.get('.nav-item.selected').should('contain', 'Search');
   });
 
   it('Rules should have the correct <title>', () => {
     cy.visit('/rules');
     cy.title().should('include', 'Ankiviewer - Rules');
-    cy.get('button').should('contain', 'Add New');
+    cy.get('.nav-item.selected').should('contain', 'Rules');
   })
 });
