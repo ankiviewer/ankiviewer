aa0bac0cd47fd409547cc722416ffc50c83c172b
samhstn <sam.houston@elixirrcreative.com>
2018-11-30 10:42:38 +0000

    move content into just state, types, view and main files

--- assets/elm/State.elm
+++ assets/elm/State.elm
@@ -4,14 +4,17 @@ import Browser
 import Browser.Navigation as Nav
 import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
+import Json.Decode.Pipeline exposing (hardcoded, optional, required)
 import List.Extra as List
-import Network.Http
 import Process
 import Set
 import Task
 import Types
     exposing
         ( Collection
+        , Card
+        , D
+        , M
         , ErrorType(..)
         , Flags
         , Model
@@ -19,13 +22,17 @@ import Types
         , Page(..)
         , RequestMsg(..)
         , Rule
+        , RuleResponse
+        , CardSearchParams
         , RuleInputType(..)
         , SyncData
         , SyncMsg(..)
         , SyncState(..)
         )
 import Url exposing (Url)
+import Url.Builder as Url
 import Url.Parser as Parser exposing (Parser, oneOf, s, top)
+import Http
 
 
 port portStartSync : Encode.Value -> Cmd msg
@@ -38,8 +45,8 @@ init : Flags -> Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
     ( initialModel url key
     , Cmd.batch
-        [ Network.Http.getCollection
-        , Network.Http.getRules
+        [ getCollection
+        , getRules
         ]
     )
 
@@ -107,7 +114,7 @@ update msg model =
             )
 
         SearchInput search ->
-            ( { model | search = search }, Network.Http.getCards { search = search } )
+            ( { model | search = search }, getCards { search = search } )
 
         RuleInput ruleInputType s ->
             ( { model | ruleInput = ruleInputUpdate model.ruleInput ruleInputType s, ruleErr = Nothing }, Cmd.none )
@@ -191,16 +198,16 @@ requestUpdate model requestMsg =
             ( model, Cmd.none )
 
         CreateRule ->
-            ( model, Network.Http.createRule model.ruleInput )
+            ( model, createRule model.ruleInput )
 
         GetRules ->
-            ( model, Network.Http.getRules )
+            ( model, getRules )
 
         UpdateRule ->
-            ( model, Network.Http.updateRule model.ruleInput )
+            ( model, updateRule model.ruleInput )
 
         DeleteRule rid ->
-            ( { model | ruleInput = Rule "" "" "" 0, selectedRule = Nothing }, Network.Http.deleteRule rid )
+            ( { model | ruleInput = Rule "" "" "" 0, selectedRule = Nothing }, deleteRule rid )
 
         NewCards (Ok cards) ->
             ( { model | cards = cards }, Cmd.none )
@@ -266,3 +273,171 @@ parser =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     portSyncMsg (SyncIncomingMsg >> Sync)
+
+
+getRules : Cmd Msg
+getRules =
+    Http.get
+        { url = "/api/rules"
+        , expect = Http.expectJson (NewRules >> Request) rulesDecoder
+        }
+
+
+createRule : Rule -> Cmd Msg
+createRule rule =
+    Http.post
+        { url = "/api/rules"
+        , body = Http.jsonBody (ruleEncoder rule)
+        , expect = Http.expectJson (NewRuleResponse >> Request) ruleResponseDecoder
+        }
+
+
+updateRule : Rule -> Cmd Msg
+updateRule rule =
+    Http.request
+        { method = "PUT"
+        , headers = []
+        , url = "/api/rules/" ++ String.fromInt rule.rid
+        , body = Http.jsonBody (ruleEncoder rule)
+        , expect = Http.expectJson (NewRuleResponse >> Request) ruleResponseDecoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+deleteRule : Int -> Cmd Msg
+deleteRule rid =
+    Http.request
+        { method = "DELETE"
+        , headers = []
+        , url = "/api/rules/" ++ String.fromInt rid
+        , body = Http.emptyBody
+        , expect = Http.expectJson (NewRules >> Request) rulesDecoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+ruleEncoder : Rule -> Encode.Value
+ruleEncoder rule =
+    Encode.object
+        [ ( "name", Encode.string rule.name )
+        , ( "code", Encode.string rule.code )
+        , ( "tests", Encode.string rule.tests )
+        ]
+
+
+rulesDecoder : Decoder (List Rule)
+rulesDecoder =
+    Decode.field "rules" (Decode.list ruleDecoder)
+
+
+ruleDecoder : Decoder Rule
+ruleDecoder =
+    Decode.succeed Rule
+        |> required "name" Decode.string
+        |> required "code" Decode.string
+        |> required "tests" Decode.string
+        |> required "rid" Decode.int
+
+
+ruleErrDecoder : Decoder Rule
+ruleErrDecoder =
+    Decode.succeed Rule
+        |> optional "name" Decode.string ""
+        |> optional "code" Decode.string ""
+        |> optional "tests" Decode.string ""
+        |> hardcoded 0
+
+
+ruleResponseDecoder : Decoder RuleResponse
+ruleResponseDecoder =
+    Decode.field "err" Decode.bool
+        |> Decode.andThen
+            (\err ->
+                if err then
+                    Decode.succeed RuleResponse
+                        |> hardcoded err
+                        |> hardcoded []
+                        |> required "params" ruleErrDecoder
+
+                else
+                    Decode.succeed RuleResponse
+                        |> hardcoded err
+                        |> required "params" (Decode.list ruleDecoder)
+                        |> hardcoded (Rule "" "" "" 0)
+            )
+
+
+getCollection : Cmd Msg
+getCollection =
+    Http.get
+        { url = "/api/collection"
+        , expect = Http.expectJson (NewCollection >> Request) collectionDecoder
+        }
+
+
+getCards : CardSearchParams -> Cmd Msg
+getCards { search } =
+    let
+        url =
+            Url.absolute
+                [ "api", "cards" ]
+                [ Url.string "search" search
+                , Url.string "model" ""
+                , Url.string "deck" ""
+                , Url.string "tags" ""
+                , Url.string "modelorder" ""
+                , Url.string "rule" ""
+                ]
+    in
+    Http.get
+        { url = url
+        , expect = Http.expectJson (NewCards >> Request) (Decode.list cardsDecoder)
+        }
+
+
+cardsDecoder : Decoder Card
+cardsDecoder =
+    Decode.succeed Card
+        |> required "model" Decode.string
+        |> required "mod" Decode.int
+        |> required "ord" Decode.int
+        |> required "tags" (Decode.list Decode.string)
+        |> required "deck" Decode.string
+        |> required "ttype" Decode.int
+        |> required "queue" Decode.int
+        |> required "due" Decode.int
+        |> required "reps" Decode.int
+        |> required "lapses" Decode.int
+        |> required "front" Decode.string
+        |> required "back" Decode.string
+
+
+collectionDecoder : Decoder Collection
+collectionDecoder =
+    Decode.succeed Collection
+        |> required "mod" Decode.int
+        |> required "cards" Decode.int
+        |> required "models" modelsDecoder
+        |> required "decks" decksDecoder
+
+
+decksDecoder : Decoder (List D)
+decksDecoder =
+    Decode.list
+        (Decode.succeed D
+            |> required "name" Decode.string
+            |> required "did" Decode.int
+        )
+
+
+modelsDecoder : Decoder (List M)
+modelsDecoder =
+    Decode.list
+        (Decode.succeed M
+            |> required "name" Decode.string
+            |> required "mid" Decode.int
+            |> required "flds" (Decode.list Decode.string)
+            |> required "did" Decode.int
+        )
--- assets/elm/View.elm
+++ assets/elm/View.elm
@@ -15,8 +15,92 @@ import Types
         , RequestMsg(..)
         , Rule
         , RuleInputType(..)
+        , SyncMsg(..)
+        , SyncState(..)
         )
-import View.Home
+import Time
+import Time.Format as Time
+
+
+homePage : Model -> Html Msg
+homePage ({ collection } as model) =
+    case model.homeMsg of
+        Err (HttpError err) ->
+            errorView <| "Http Error: " ++ err
+
+        Err (SyncError err) ->
+            errorView <| "Sync Error: " ++ err
+
+        Ok (Syncing ( message, syncPercentage )) ->
+            syncing
+                { message = message
+                , syncPercentage = syncPercentage
+                }
+
+        Ok NotSyncing ->
+            info
+                { mod = collection.mod
+                , cards = collection.cards
+                }
+
+
+errorView : String -> Html Msg
+errorView errorText =
+    div
+        [ class "red" ]
+        [ text errorText
+        ]
+
+
+info : { mod : Int, cards : Int } -> Html Msg
+info { mod, cards } =
+    div
+        []
+        [ div
+            [ class "mv2"
+            , id "home-last_modified"
+            ]
+            [ text <| "Last modified: " ++ Time.format Time.utc "Weekday, ordDay Month Year at padHour:padMinute" mod
+            ]
+        , div
+            [ class "mv2"
+            , id "home-number_notes"
+            ]
+            [ text <| "Number notes: " ++ String.fromInt cards
+            ]
+        , button
+            [ onClick <| Sync StartSync
+            , class "button-primary"
+            , id "home-sync_button"
+            ]
+            [ text "Sync Database"
+            ]
+        ]
+
+
+syncing : { message : String, syncPercentage : Int } -> Html Msg
+syncing { message, syncPercentage } =
+    div
+        []
+        [ div
+            []
+            [ text <| message ++ "..."
+            ]
+        , div
+            [ class "sync-loader"
+            , id "home-sync_loader"
+            ]
+            [ div
+                [ class "sync-bar"
+                , style "width" <| String.fromInt syncPercentage ++ "%"
+                ]
+                []
+            ]
+        , div
+            []
+            [ text <| String.fromInt syncPercentage ++ "%"
+            ]
+        ]
 
 
 cardColumns : List String
@@ -59,7 +143,7 @@ body model =
         }
     , case model.page of
         Home ->
-            View.Home.view model
+            homePage model
 
         Search ->
             searchPage model
