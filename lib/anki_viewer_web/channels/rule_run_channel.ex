3e4cbf96d442a586cb3a06ca77c989c7763af282
samhstn <shouston3@me.com>
2018-12-08 09:53:01 +0000

    implement rule running

---
+++ lib/anki_viewer_web/channels/rule_run_channel.ex
@@ -0,0 +1,39 @@
+defmodule AnkiViewerWeb.RuleRunChannel do
+  use AnkiViewerWeb, :channel
+
+  def join("run:rule", %{"rid" => rid}, socket) do
+    IO.inspect label: "rid" rid
+
+    send(self(), {:run, :rule, %{rid: rid}})
+
+    {:ok, socket}
+  end
+
+  def handle_info({:run, :rule, %{rid: rid}}, socket) do
+    push(socket, "run:msg", %{msg: "starting run"})
+
+    notes = Repo.all(Note)
+    rule = Repo.get(Rule, rid)
+
+    for n <- notes do
+      %NoteRule{nid: n.nid, rid: rid}
+      |> Map.merge(
+        case NoteRule.run(notes, n, rule) do
+          :ok ->
+            %{fails: false}
+
+          {:error, ""} ->
+            %{fails: true}
+
+          {:error, solution} ->
+            %{fails: true, solution: solution}
+        end
+      )
+      |> NoteRule.insert!()
+    end
+
+    push(socket, "done", %{})
+
+    {:noreply, socket}
+  end
+end
